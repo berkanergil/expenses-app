ec4b5e58e5e5e3e723ddf5877935249f8dbfe120
Your Name <you@example.com>
2023-01-11 10:18:16 +0200

    expense date item cretaed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.02,
+      date: new Date(2022, 1, 2),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 924.02,
+      date: new Date(2022, 1, 2),
+    },
+    {
+      id: "e3",
+      title: "New Desk",
+      amount: 94.02,
+      date: new Date(2022, 1, 2),
+    },
+  ];
   return (
     <div>
-      <h2>Hello World !</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
